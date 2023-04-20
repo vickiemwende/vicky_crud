fd2f98bc5ae8ae82fc60a73534c52387d5675944
eMobilis <you@example.com>
2023-04-20 15:03:13 +0300

    first commit

---
+++ IdeaProjects/kotlin_wikwan/src/main/kotlin/Datatypes.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    //Int
+    //Strings
+    //Char____use single quote
+    //Boolean
+    //Float
+    //Double
+    var City: String = "Nairobi"//String
+    var Age : Int =20 //int
+    var domain :Char = 'K'//Char
+    var status : Boolean =true //Boolean
+    var temperature : Float =35.9F // Float
+    var  result :Double = 23.54678 // Double
+}
